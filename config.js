d0db4a5d5ca5735260191b9ce5aa81cb87dc9784
Sumit-Malviya <codewithsumit0611@gmail.com>
2024-01-02 12:28:29 +0530

    New contribution

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 console.log('config.js');
 console.log('version13');
+console.log("My first contribution to team's project");
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log('index.js');
 console.log('version13');
+console.log("My first contribution to team's project");
